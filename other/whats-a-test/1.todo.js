721b3682d887b6d750d7591b6133876393af48d4
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-11-27 09:55:03 +0000

    completed first exercise

--- other/whats-a-test/1.todo.js
+++ other/whats-a-test/1.todo.js
@@ -19,5 +19,14 @@ error if the subtract function were to have a bug
 */
 
 // sum is intentionally broken so you can see errors in the tests
-const sum = (a, b) => a - b
+const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
+
+if (sum(3,3) !== 6) throw new Error('Sum is not adding the numbers correctly.')
+
+let result = subtract(10,3)
+let expected = 7;
+
+if (result !== expected) {
+  throw new Error(`The result of ${result} does not equal the expected output of ${expected}`)
+}
