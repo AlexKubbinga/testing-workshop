57b0b96f1bbd08c1e967055fd124f660cfd3f85e
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-11-27 14:57:36 +0000

    wrote test for user auth

--- server/src/utils/__tests__/auth.todo.js
+++ server/src/utils/__tests__/auth.todo.js
@@ -1,8 +1,26 @@
+import {isPasswordAllowed, userToJSON} from '../auth'
 test('isPasswordAllowed only allows some passwords', () => {
-  // here's where I'll demo things for you :)
+  expect(isPasswordAllowed('')).toBe(false)
+  expect(isPasswordAllowed('AAAAAAAAA')).toBe(false)
+  expect(isPasswordAllowed('9999999999')).toBe(false)
+  expect(isPasswordAllowed('sakjdfkl21398')).toBe(true)
 })
 
 test('userToJSON excludes secure properties', () => {
+  const validUser = {
+    id: 'some-id',
+    username: 'sarah',
+  }
+  const invalidUser = {
+    ...validUser,
+    exp: new Date(),
+    iat: new Date(),
+    hash: 'some really long string',
+    salt: 'some shorter string',
+  }
+
+  expect(userToJSON(invalidUser)).toEqual(validUser)
+  expect(userToJSON(validUser)).toEqual(validUser)
   // Here you'll need to create a test user object
   // pass that to the userToJSON function
   // and then assert that the test user object
@@ -12,29 +30,4 @@ test('userToJSON excludes secure properties', () => {
   // const user = {
   //   id: 'some-id',
   //   username: 'sarah',
-  //   // ↑ above are properties which should
-  //   // be present in the returned object
-  //
-  //   // ↓ below are properties which shouldn't
-  //   // be present in the returned object
-  //   exp: new Date(),
-  //   iat: new Date(),
-  //   hash: 'some really long string',
-  //   salt: 'some shorter string',
-  // }
 })
-
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=Testing&e=auth%20util&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
-////////////////////////////////
